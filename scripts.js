2c958ed0c8da489fcdfda9de4745ccc671dae353
Marcus Jeong <marcus.jeong96@gmail.com>
2024-11-01 00:03:08 -0400

    got an array with rgb info on each pixel of image

--- scripts.js
+++ scripts.js
@@ -1,34 +1,40 @@
 const test = document.getElementById("test");
 const img1 = document.getElementById("img_split");
+let arr = [];
 // every pixel image is a collection of pixels; as in a 320x320 image has 1 color per pixel for 320x320 items
 // make a fxn that gets the color of each pixel
-function draw(img) {
-    var canvas = document.createElement("canvas");
-    //create canvas
-    var c = canvas.getContext('2d');
-    //get height and width
-    c.width = canvas.width = img.width;
-    c.height = canvas.height = img.height;
-    //supplant the img onto the canvas
-    c.drawImage(img1, 0, 0);
-    // get each pixel
-    var x = 100;
-    var y = 100;
-    var imageData = c.getImageData(0, 0, 10, 10);
-    console.log(imageData.data);
-    var index = (x + y * imageData.width) * 4;
-    var r = imageData.data[index];
-    var g = imageData.data[index + 1];
-    var b = imageData.data[index + 2];
-    var a = imageData.data[index + 3];
-    console.log(`RGBA:(${r},${g},${b},${a})`)
-
-    var rowStart = y * imageData.width * 4;
-    var pixelIndex = rowStart + x * 4;
-    console.log(pixelIndex);
-    return c;
+function main() {
+    //make a canvas of the image and get the image data
+    const canvas = document.getElementById("canvas");
+    canvas.width = img1.width;
+    canvas.height = img1.height;
+    const ctx = canvas.getContext("2d");
+    ctx.drawImage(img1, 0, 0);
+    
+    const imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    return imageData;
 };
 
+function buildRGB(imageData) {
+    // make an empty array
+    const rgbValues = [];
+    // loop through imagedata and get the rgb values for each 
+    for(let i = 0; i < imageData.length; i += 4) {
+        const rgb = {
+            r: imageData[i],
+            g: imageData[i + 1],
+            b: imageData[i + 2],
+        };
+        rgbValues.push(rgb);
+    }
+    return rgbValues;
+}
+
 test.addEventListener("click", () => {
-    draw(img1);
+    //get the image data
+    let imageD = main();
+    console.log(imageD);
+    // get an array with all the rgb values of the image
+    arr = buildRGB(imageD.data);
+    console.log(arr);
 });
